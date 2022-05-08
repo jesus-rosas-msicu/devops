c243780c8acd1f2867fa6443bbf530a135fbfb92
jesus-rosas-msicu <themexicanceo2@outlook.com>
2022-05-08 18:27:15 -0500

    Third Commit in the repository

--- automaticrepo.sh
+++ automaticrepo.sh
@@ -6,3 +6,9 @@ git commit -m "First Commit"
 git branch -M main
 git remote add origin https://github.com/jesus-rosas-msicu/devops.git
 git push -u origin main
+#git config --list
+#Configuration list
+#
+#git config --global user.name "alan rodriguez"
+#git config --global user.email  "themexicanceo@gmail.com"
+
